29f597713978eecd0b3cd13c50b6bb7bae52e02b
ashtav <govins.com@gmail.com>
2021-01-07 00:07:02 +0800

    new ui and feature

---
+++ js/coderator.js
@@ -0,0 +1,51 @@
+const doc = document;
+
+function camelize (dasherizedStr) {
+    return dasherizedStr
+    .replace(/_([a-zA-Z])/g, function (m1, m2) {
+    return m2.toUpperCase()
+    });
+}
+
+class Coderator {
+
+    // Map<String, dynamic>
+    toMap(params){
+        let res = '',
+            split = params.value.replace(/ /g,'').split(',')
+
+            for (let i = 0; i < split.length; i++) {
+                let s = '"'+split[i]+'": '+camelize(split[i])+'.text, '
+                res += s
+            }
+
+            doc.querySelector(params.target).innerText = 'Map<String, dynamic> formData = {'+res.replace(/,\s*$/, "")+'}';
+    }
+
+    // TextEditingController
+    toTec(params){
+        let res = '',
+            split = params.value.replace(/ /g,'').split(',')
+
+        for (let i = 0; i < split.length; i++) {
+            let s = camelize(split[i])+' = TextEditingController(), '
+            res += s
+        }
+
+        doc.querySelector(params.target).innerText = 'TextEditingController '+res.replace(/,\s*$/, "")+';'
+    }
+
+    // FocusNode
+    toFn(params){
+        let res = '',
+            split = params.value.replace(/ /g,'').split(',')
+
+        for (let i = 0; i < split.length; i++) {
+            let s = camelize(split[i])+'Node = FocusNode(), '
+            // res += 'node'+s[0].toLocaleUpperCase()+(s.substring(1))
+            res += s
+        }
+
+        doc.querySelector(params.target).innerText = 'FocusNode '+res.replace(/,\s*$/, "")+';'
+    }
+}
